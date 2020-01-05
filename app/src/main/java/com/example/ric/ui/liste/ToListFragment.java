c2f92366abbf18cf2c2566b6a2ec06196f17ee9f
GUILLEMEAU FLORIAN <floguild68@hotmail.fr>
2020-01-05 16:00:52 +0100

    Ajout de fragment dans la navigation

---
+++ app/src/main/java/com/example/ric/ui/liste/ToListFragment.java
@@ -0,0 +1,25 @@
+package com.example.ric.ui.liste;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.ric.R;
+
+public class ToListFragment extends Fragment {
+
+    public ToListFragment() { }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_temp, container, false);
+    }
+}
